b7bdc25d49f499f411ce2e0bfc79e25e776dc749
Derian Pinto <pintoderian@gmail.com>
2022-02-23 23:05:55 -0500

    Añadiendo header

--- src/app.js
+++ src/app.js
@@ -20,7 +20,14 @@ app.post('/ninja', function (req, res) {
   const url = req.body.url;
   
   puppeteer.launch().then(async function (browser) {
-    const page = await browser.newPage();
+    const [page] = await browser.pages();
+    await page.setExtraHTTPHeaders({
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+        'upgrade-insecure-requests': '1',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,en;q=0.8'
+    })
     await page.goto(url, { waitUntil: 'networkidle0' });
     const bodyHTML = await page.evaluate(() => document.documentElement.innerHTML);
     await browser.close();
